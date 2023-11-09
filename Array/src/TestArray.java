7226f725f35079a1f1448b6681429f9f6dccd16f
rasik26 <47294302+rasik26@users.noreply.github.com>
2023-11-09 10:41:06 +0530

    Update TestArray.java

--- Array/src/TestArray.java
+++ Array/src/TestArray.java
@@ -49,3 +49,39 @@ public class TestArray {
 	 }
 
 }
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Animal {
+    // Some properties and methods common to all animals
+}
+
+class Cage<T extends Animal> {
+    private List<? super T> animals;
+
+    public Cage() {
+        this.animals = new ArrayList<>();
+    }
+
+    public void addAnimal(T animal) {
+        animals.add(animal);
+    }
+
+    public void displayAnimals() {
+        for (Object animal : animals) {
+            System.out.println(animal);
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Cage<Animal> animalCage = new Cage<>();
+        animalCage.addAnimal(new Animal());
+        animalCage.addAnimal(new Animal());
+        animalCage.displayAnimals();
+    }
+}
+
